fa1952abba77bcbd9293399c54950595d3a1bbf2
emmisk <koistinenemmisofia@gmail.com>
2020-07-02 15:57:57 +0300

    edit hovers

--- src/App.js
+++ src/App.js
@@ -5,13 +5,13 @@ import face from "./assets/images/face.svg"
 import artGallery from "./assets/images/artGallery.svg"
 
 const App = () => (
-  <div className=" ">
+  <div className="">
     <div className="p-2 text-black  text-5xl text-center uppercase">
       <Header />
     </div>
     <div className="md:flex mt-2">
       <div className="md:flex-shrink-0 md:w-1/2">
-        <div className="md:w-full shadow-inner transition duration-500 ease-in-out bg-red-200 hover:bg-red-300 transform hover:-translate-y-1 hover:scale-110 ...">
+        <div className="md:w-full shadow-inner transition duration-500 ease-in-out bg-red-200 hover:bg-red-300 transform hover:-translate-y-1 hover:scale-90 ...">
           <Section
             image={face}
             href="https://emmikcv.netlify.app/"
@@ -19,16 +19,16 @@ const App = () => (
           />
         </div>
       </div>
-      <div className="grid-cols-3 gap-4 mt-4 md:mt-0 mb-2 md:ml-2 ">
+      <div className="grid-cols-3 gap-4 mt-4 md:mt-0 mb-2 md:ml-2">
         <p className="text-gray-700 text-center bg-teal-300 p-8 shadow-inner">
           Hey, I'm Emmi! This is my portfolio page. You can find my resume and
           projects over here.
         </p>
-        <div className="grid grid-cols-3 mr-2 mt-4">
+        <div className="grid grid-cols-3 mr-4 mt-4 ml-4">
           <div className="col-span-1 ... text-gray-700 text-center ml-2 bg-yellow-300 p-2 shadow-inner">
             <p className="mt-14">React App. Gallery for my art work. -></p>
           </div>
-          <div class="mr-4 col-span-1 col-span-2 transition duration-500 ease-in-out bg-yellow-400 hover:bg-yellow-500 transform hover:-translate-y-1 hover:scale-110 ...">
+          <div class="mr-4 col-span-1 col-span-2 transition duration-500 ease-in-out bg-yellow-400 hover:bg-yellow-500 transform hover:-translate-y-1 hover:scale-90 ...">
             <Section
               image={artGallery}
               href="https://emmiart.netlify.app/"
@@ -36,11 +36,11 @@ const App = () => (
             />
           </div>
         </div>
-        <div className="grid grid-cols-3 gab-2 mt-2 ml-2 h-24 mr-2">
+        <div className="grid grid-cols-3 gab-2 mt-2 ml-2 h-24 ">
           <div className="col-span-1 col-span-2 ... text-black-700 text-center bg-green-200 p-2 shadow-inner">
             <p>test</p>
           </div>
-          <div className="col-span-1 ... text-gray-700 text-center ml-2 mr-4 bg-orange-400 p-2 shadow-inner">
+          <div className="col-span-1 ... text-gray-700 text-center ml-2 mr-4 bg-red-200 p-2 shadow-inner">
             <p className="mt-14">test</p>
           </div>
         </div>
