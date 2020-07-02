04555118a845a8a00b0d5c2f0d3d718d61482973
emmisk <koistinenemmisofia@gmail.com>
2020-07-02 18:53:07 +0300

    add text and link

--- src/App.js
+++ src/App.js
@@ -20,15 +20,30 @@ const App = () => (
         </div>
       </div>
       <div className="grid-cols-3 gap-4 mt-4 md:mt-0 mb-2 ">
-        <p className="text-gray-700 text-center bg-teal-300 p-8 shadow-inner">
+        <p className="text-gray-800 text-xl text-center bg-teal-300 p-8 shadow-inner">
           Hey, I'm Emmi! This is my portfolio page. You can find my resume and
           projects over here.
         </p>
         <div className="grid grid-cols-3 mr-2 mt-2 ml-2">
-          <div className="col-span-1 ... text-gray-700 text-center bg-yellow-300 p-2 shadow-inner">
-            <p className="mt-14">React App. Gallery for my art work. -></p>
+          <div className="col-span-1 ... text-black text-center bg-yellow-300 p-2 shadow-inner">
+            <div className="grid-cols-1">
+              <p className="text-xl pl-2 mt-2 uppercase">
+                Gallery for my art work
+              </p>
+              <p className="text-l pl-2 mt-2">React App</p>
+              <p className="text-xl pl-2 mt-2"></p>
+              <div class="text-white rounded-full flex items-center justify-center transition duration-500 ease-in-out bg-black hover:bg-yellow-500 transform hover:-translate-y-1 hover:scale-110 ...">
+                <a
+                  href="https://github.com/emmisk/art_gallery"
+                  target="_blank"
+                  rel="noreferrer noopener"
+                >
+                  GitHub
+                </a>
+              </div>
+            </div>
           </div>
-          <div class="mr-2 col-span-1 col-span-2 transition duration-500 ease-in-out bg-yellow-400 hover:bg-yellow-500 transform hover:-translate-y-1 hover:scale-90 ...">
+          <div class="col-span-1 col-span-2 transition duration-500 ease-in-out bg-yellow-400 hover:bg-yellow-500 transform hover:-translate-y-1 hover:scale-90 ...">
             <Section
               image={artGallery}
               href="https://emmiart.netlify.app/"
@@ -37,11 +52,13 @@ const App = () => (
           </div>
         </div>
         <div className="grid grid-cols-3 gab-2 mt-2 ml-2 h-24 ">
-          <div className="col-span-1 col-span-2 ... text-black-700 text-center bg-green-200 p-2 shadow-inner">
-            <p>test</p>
+          <div className="col-span-1 col-span-2 ... text-center bg-green-200 shadow-inner">
+            <p className="mt-4 text-green-800 text-lg">
+              You can find my contact information in my resume.
+            </p>
           </div>
-          <div className="col-span-1 ... text-gray-700 text-center ml-2 mr-4 bg-red-200 p-2 shadow-inner">
-            <p className="mt-14">test</p>
+          <div className="col-span-1 ... text-gray-700 text-center ml-2 mr-2 bg-red-200 p-2 shadow-inner">
+            <p className="p-2">Thank you for your visit!</p>
           </div>
         </div>
       </div>
