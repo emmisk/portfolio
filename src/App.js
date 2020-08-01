02232565b5808f121f551a9198df91e5558af539
emmisk <koistinenemmisofia@gmail.com>
2020-08-01 18:19:19 +0300

    move tailwind.config to root and other fixes

--- src/App.js
+++ src/App.js
@@ -23,14 +23,14 @@ const App = () => (
           projects over here.
         </p>
         <div className="grid grid-cols-3 mr-2 mt-2 ml-2">
-          <div className="col-span-1 ... text-black text-center bg-yellow-300 p-2 shadow-inner">
+          <div className="col-span-1 text-black text-center bg-yellow-300 p-2 shadow-inner">
             <div className="grid-cols-1">
               <p className="text-xl pl-2 mt-2 uppercase">
                 Gallery for my art work
               </p>
               <p className="text-l pl-2 mt-2">React App</p>
               <p className="text-xl pl-2 mt-2"></p>
-              <div className="text-white rounded-full flex items-center justify-center transition duration-500 ease-in-out bg-black hover:bg-yellow-500 transform hover:-translate-y-1 hover:scale-110 ...">
+              <div className="text-white rounded-full flex items-center justify-center transition duration-500 ease-in-out bg-black hover:bg-yellow-500 transform hover:-translate-y-1 hover:scale-110">
                 <a
                   href="https://github.com/emmisk/art_gallery"
                   target="_blank"
@@ -49,13 +49,13 @@ const App = () => (
             hoverColor="bg-yellow-500"
           />
         </div>
-        <div className="grid grid-cols-3 gab-2 mt-2 ml-2  ">
-          <div className="col-span-1 col-span-2 ... text-center bg-green-200 shadow-inner">
+        <div className="grid grid-cols-3 gab-2 mt-2 ml-2">
+          <div className="col-span-1 col-span-2 text-center bg-green-200 shadow-inner">
             <p className="mt-4 text-green-800 text-lg">
               You can find my contact information in my resume.
             </p>
           </div>
-          <div className="col-span-1 ... text-gray-700 text-center ml-2 mr-2 p-2 bg-red-200  shadow-inner">
+          <div className="col-span-1 text-gray-700 text-center ml-2 mr-2 p-2 bg-red-200 shadow-inner">
             <p className="p-2">Thank you for your visit!</p>
           </div>
         </div>
