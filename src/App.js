aff4660d6b9a0b0914c97eacde6e06765572bcf3
emmisk <koistinenemmisofia@gmail.com>
2020-08-04 14:23:39 +0300

    add suspense

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { Suspense } from "react"
 import Header from "./components/Header.js"
 import Resume from "./components/Resume.js"
 import Introduction from "./components/Introduction.js"
@@ -10,32 +10,34 @@ import artGallery from "./assets/images/artGallery.svg"
 
 const App = () => (
   <>
-    <Header />
-    <div className="md:flex mt-2">
-      <div className="md:flex-shrink-0 md:w-1/2">
-        <Resume
-          image={face}
-          href="https://emmikcv.netlify.app/"
-          alt="My face"
-        />
+    <Suspense fallback={<div>Loading...</div>}>
+      <Header />
+      <div className="md:flex mt-2">
+        <div className="md:flex-shrink-0 md:w-1/2">
+          <Resume
+            image={face}
+            href="https://emmikcv.netlify.app/"
+            alt="My face"
+          />
+        </div>
+        <div className="grid-cols-3 gap-4 mt-4 md:mt-0 mb-2">
+          <Introduction />
+          <Project
+            projectHeader="Gallery for my art work"
+            projectType="React App"
+            projectColor="bg-yellow-300"
+            projectHover="bg-yellow-500"
+            image={artGallery}
+            href="https://emmiart.netlify.app/"
+            alt="My face"
+            imageColor="bg-yellow-400"
+            imageHover="bg-yellow-500"
+          />
+          <ContactInfo />
+        </div>
       </div>
-      <div className="grid-cols-3 gap-4 mt-4 md:mt-0 mb-2">
-        <Introduction />
-        <Project
-          projectHeader="Gallery for my art work"
-          projectType="React App"
-          projectColor="bg-yellow-300"
-          projectHover="bg-yellow-500"
-          image={artGallery}
-          href="https://emmiart.netlify.app/"
-          alt="My face"
-          imageColor="bg-yellow-400"
-          imageHover="bg-yellow-500"
-        />
-        <ContactInfo />
-      </div>
-    </div>
-    <Footer />
+      <Footer />
+    </Suspense>
   </>
 )
 
