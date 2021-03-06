ff384040157b2e86a61fff62ee144694c7f4189d
emmisk <koistinenemmisofia@gmail.com>
2021-03-06 16:15:47 +0200

    add Image.js

---
+++ src/components/Image.js
@@ -0,0 +1,12 @@
+import React from "react"
+import MyImg from "../assets/images/me.jpg"
+
+const Image = ({}) => {
+    return (
+        <>
+        <img className=" w-full" src={MyImg} alt="me"/>
+        </>
+        )
+    }
+
+export default Image
