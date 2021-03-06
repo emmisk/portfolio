3fd602ab8bf72c5470708870166a09db20838d17
emmisk <koistinenemmisofia@gmail.com>
2021-03-06 16:16:07 +0200

    edit className

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,7 @@
 import React from "react"
 
 const Header = () => (
-  <h1 className="p-2 text-black text-5xl text-center uppercase">Portfolio</h1>
+  <h1 className="p-2 text-black text-5xl font-mono text-center uppercase">Emmi Koistinen</h1>
 )
 
 export default Header
