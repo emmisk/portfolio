330d521dde04893a9974777d53cc54fc83edf065
emmisk <koistinenemmisofia@gmail.com>
2021-03-06 16:04:39 +0200

    add InfoText.js

---
+++ src/components/InfoText.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+const InfoText = ({infoText}) => {
+    return (
+        <>
+        <p className="flex-auto text-center font-mono">{infoText}</p>
+        </>
+        )
+    }
+
+export default InfoText
