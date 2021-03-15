08f4c764aacbafb7559d30aafc7b5c26f90455c6
emmisk <koistinenemmisofia@gmail.com>
2021-03-15 13:40:42 +0200

    add location

--- src/components/SubHeader.js
+++ src/components/SubHeader.js
@@ -1,11 +1,12 @@
 import React from "react"
 
-const SubHeader = ({job, titleAndDate, infoText}) => {
+const SubHeader = ({job, location, titleAndDate, infoText}) => {
     return (
         <>
         <p className="flex-auto text-center font-mono md:w-full font-bold text-blue-700">{job}</p>
+        <p className="flex-auto text-center font-mono font-medium text-blue-600">{location}</p>
         <p className="flex-auto text-center font-mono font-medium text-blue-500">{titleAndDate}</p>
-        <p className="flex-auto text-center font-mono">{infoText}</p>
+        <p className="flex-auto text-center font-mono ml-4 mr-4">{infoText}</p>
         </>
         )
     }
