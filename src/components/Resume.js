dc80ad246e148c0732bcb3bf319ccb39c3278bec
emmisk <koistinenemmisofia@gmail.com>
2021-03-15 14:02:47 +0200

    add button

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,12 +1,14 @@
 import React from "react"
 
-const Resume = ({ image, text, href, alt }) => (
-  <section className="md:w-full shadow-inner transition bg-red-300 hover:bg-red-400">
-    <a href={href} target="_blank" rel="noreferrer noopener">
+const Resume = ({ image, text, href, alt, handleClick }) => (
+  <div className="sm:w-auto">
+  <button onClick={handleClick} className="md:w-full w-full focus:outline-none shadow-inner transition bg-red-300 hover:bg-red-400">
+      <a href={href} target="_blank" rel="noreferrer noopener">
       <img src={image} alt={alt} />
     </a>
     <p>{text}</p>
-  </section>
+  </button>
+  </div>
 )
 
 export default Resume
