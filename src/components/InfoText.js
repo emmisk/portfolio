e9274ff13740f35946ece882fb7adcf1b1a3d386
emmisk <koistinenemmisofia@gmail.com>
2021-03-15 13:45:24 +0200

    add margin

--- src/components/InfoText.js
+++ src/components/InfoText.js
@@ -3,7 +3,7 @@ import React from "react"
 const InfoText = ({infoText}) => {
     return (
         <>
-        <p className="flex-auto text-center font-mono">{infoText}</p>
+        <p className="flex-auto text-center font-mono ml-4 mr-4">{infoText}</p>
         </>
         )
     }
