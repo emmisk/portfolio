c77d60a604aa1ed6c21a0c9e3a9210692d7bdb2c
emmisk <koistinenemmisofia@gmail.com>
2021-03-15 14:04:36 +0200

    remove Button and Info, add ScrollDown

--- src/App.js
+++ src/App.js
@@ -7,74 +7,22 @@ import ContactInfo from "./components/ContactInfo.js"
 import Footer from "./components/Footer.js"
 import face from "./assets/images/face.svg"
 import artGallery from "./assets/images/artGallery.svg"
-import MyImg from "./assets/images/me.jpg"
-
-
-const Button = ({ handleClick, text }) => (
-  <button onClick={handleClick}>{text}</button>
-)
-const Info = () => {
-  // const scrollTop = () => window.scrollTo({ top: 0, behavior: "smooth" })
-  const scrollTop = () => window.scrollTo({ top: document.scrollingElement.scrollHeight, behavior: "smooth" })
-return (
-
-  <div className="w-full grid-cols-3 gap-4 mt-4 md:mt-0 mb-2">
-    <br />
-    <p className="flex-auto text-center font-mono md:w-full font-bold text-2xl text-red-500">
-    Who am I?
-    </p>
-    <br />
-    <p className="flex-auto text-center font-mono">I'm Emmi Koistinen and I living in Helsinki, Finland. 
-    I'm person who inspired by new things all the time and I always want to improve myself.
-    I do my job carefully and always try to do my best. I like to solve problems and design different 
-    things. 
-    
-    </p>
-    <br />
-    <p className="flex-auto text-center font-mono md:w-full font-bold text-2xl text-red-500">
-    My skills
-    </p>
-    <br />
-    <p className="flex-auto text-center font-mono">Programming languages: JavaScript(React), Java and SQL. </p>
-    <p className="flex-auto text-center font-mono">Test automation: JUnit and Selenium. </p>
-    <p className="flex-auto text-center font-mono">Marketing and analytics: SEO and making dashboards. </p>
-    <p className="flex-auto text-center font-mono">Databases: PostgreSQL. </p>
-    <p className="flex-auto text-center font-mono">Extra skills: google searching. </p>
-    <br />
-    <p className="flex-auto text-center font-mono md:w-full font-bold text-2xl text-red-500">
-    Work History
-    </p>
-    <br />
-    <p className="flex-auto text-center font-mono md:w-full font-bold text-blue-700">STUK - Säteilyturvakeskus</p>
-    <p className="flex-auto text-center font-mono font-medium text-blue-500">System designer 10/2020 - 4/2021</p>
-    <p className="flex-auto text-center font-mono">I made test automation with Java, JUnit and Selenium.</p>
-    <br/>
-    <p className="flex-auto text-center font-mono md:w-full font-bold text-blue-700">Isolta</p>
-    <p className="flex-auto text-center font-mono font-medium text-blue-500">Marketin analyst 8/2018 - 1/2019</p>
-    <p className="flex-auto text-center font-mono">I was part of marketing team and made dashboards about marketing data
-    with my teammate.</p>
-    <br />
-    <p className="flex-auto text-center font-mono md:w-full font-bold text-2xl text-red-500">
-    My hobbies
-    </p>
-    <p className="flex-auto text-center font-mono">My favorite hobbies are cycling, painting and playing on Xbox.</p>
-    <br />
-    <img className="object-none object-center " src={MyImg} alt="me"/>
-    {/* <Button handleClick={scrollTop} text="Scroll top" /> */}
-  </div>
-)
-}
+import CvInfo from "./components/CvInfo.js"
 
 const App = () => {
   const [show, setShow] = useState(null)
 
   const handleClick = () => {
-    toggleInfo()
-    scrollTop()
+    if(show){
+      toggleInfo()
+    }else{
+      toggleInfo()
+      scrollDown()
+    }
   } 
 
   const toggleInfo = () => (show === "info" ? setShow(null) : setShow("info"))
-  const scrollTop = () => window.scrollTo({ top: document.scrollingElement.scrollHeight, behavior: "smooth" })
+  const scrollDown = () => window.scrollTo({ top: document.scrollingElement.scrollHeight, behavior: "smooth" })
 
   return (
   <>
@@ -104,7 +52,7 @@ const App = () => {
         </div>
       </div>
       <div className="">
-            {show === "info" && <Info />}
+            {show === "info" && <CvInfo />}
       </div>
       <Footer />
     </Suspense>
