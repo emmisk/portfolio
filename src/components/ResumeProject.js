49cd920c6fb7726f7581a2ba1186ffe0a8172179
emmisk <koistinenemmisofia@gmail.com>
2021-03-23 14:11:42 +0200

    add resumeProject

---
+++ src/components/ResumeProject.js
@@ -0,0 +1,33 @@
+import React from "react"
+
+const ResumeProject = ({
+  alt,
+  projectColor,
+  projectHeader,
+  projectType,
+  image,
+  handleClick
+}) => (
+  <div className="">
+    <div
+      className={`col-span-1 text-black text-center ${projectColor} shadow-inner`}
+    >
+  <button onClick={handleClick} className="w-full focus:outline-none shadow-inner transition bg-red-300 hover:bg-red-400">
+      <img src={image} alt={alt} />
+  </button>
+        <div className="grid-cols-1">
+        <p className="text-xl uppercase">{projectHeader}</p>
+        <p className="text-l mt-2 mb-2">{projectType}</p>
+          <a
+            href="https://github.com/emmisk/portfolio"
+            target="_blank"
+            rel="noreferrer noopener"
+          >
+            <button className="w-full text-white rounded-full flex items-center justify-center transition duration-200 ease-in-out bg-black hover transform hover:-translate-y-1 hover:scale-110">GitHub</button>
+          </a>
+      </div>
+    </div>
+  </div>
+)
+
+export default ResumeProject
