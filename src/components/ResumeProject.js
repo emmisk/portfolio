2143f68535a279ac13ebb58622e41dd91bd4931a
emmisk <koistinenemmisofia@gmail.com>
2021-03-23 19:08:35 +0200

    WIP

--- src/components/ResumeProject.js
+++ src/components/ResumeProject.js
@@ -6,26 +6,17 @@ const ResumeProject = ({
   projectHeader,
   projectType,
   image,
-  handleClick
+  handleClick,
+  href
 }) => (
-  <div className="">
+
+  <div className="grid grid-cols-1">
     <div
-      className={`col-span-1 text-black text-center ${projectColor} shadow-inner`}
+      className={`col-span-1 text-black text-center  shadow-inner`}
     >
-  <button onClick={handleClick} className="w-full focus:outline-none shadow-inner transition bg-red-300 hover:bg-red-400">
+  <button onClick={handleClick} className="w-full md:w-5/6 focus:outline-none shadow-inner transition bg-red-300 hover:bg-red-400">
       <img src={image} alt={alt} />
   </button>
-        <div className="grid-cols-1">
-        <p className="text-xl uppercase">{projectHeader}</p>
-        <p className="text-l mt-2 mb-2">{projectType}</p>
-          <a
-            href="https://github.com/emmisk/portfolio"
-            target="_blank"
-            rel="noreferrer noopener"
-          >
-            <button className="mb-2 w-full text-white rounded-full flex items-center justify-center transition duration-200 ease-in-out bg-black hover transform hover:-translate-y-1 hover:scale-110">GitHub</button>
-          </a>
-      </div>
     </div>
   </div>
 )
