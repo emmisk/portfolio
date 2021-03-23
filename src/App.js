2021b2830744216303d321b26c3431028c888183
emmisk <koistinenemmisofia@gmail.com>
2021-03-23 14:18:25 +0200

    remove Resume and ContactInfo, edit css

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React, { Suspense, useState } from "react"
 import Header from "./components/Header.js"
-import Resume from "./components/Resume.js"
 import Introduction from "./components/Introduction.js"
 import Project from "./components/Project.js"
-import ContactInfo from "./components/ContactInfo.js"
 import Footer from "./components/Footer.js"
 import face from "./assets/images/face.svg"
 import artGallery from "./assets/images/artGallery.svg"
 import CvInfo from "./components/CvInfo.js"
+import ResumeProject from "./components/ResumeProject.js"
 
 const App = () => {
   const [show, setShow] = useState(null)
@@ -27,16 +26,18 @@ const App = () => {
   return (
   <>
     <Suspense fallback={<div>Loading...</div>}>
-      <Header />
-      <div className="md:flex mt-2">
-        <div className="md:flex-shrink-0 md:w-1/2">
-          <Resume handleClick={handleClick} 
-            image={face}
-            // href="https://emmikcv.netlify.app/"
-            alt="My face"
-            />
-        </div>
-        <div className="grid-cols-3 gap-4 mt-4 md:mt-0 mb-2">
+      <Header/>
+      <div className="md:flex">
+        <div className="md:w-1/2 ">
+            <ResumeProject
+              projectHeader="Portfolio"
+              projectType="React, Tailwind CSS"
+              projectColor="bg-red-200"
+              image={face}
+              handleClick={handleClick} 
+          />
+          </div>
+        <div className="grid-cols-3 gap-2">
           <Introduction />
           <Project
             projectHeader="Gallery for my art work"
@@ -46,9 +47,8 @@ const App = () => {
             href="https://emmiart.netlify.app/"
             alt="My face"
             imageColor="bg-yellow-500"
-            hoverColor="bg-yellow-600"
+            // hoverColor="bg-yellow-600"
             />
-          <ContactInfo />
         </div>
       </div>
       <div className="">
