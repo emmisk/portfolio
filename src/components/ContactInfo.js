7321952e3ae9b638aada8061b020a6c1fcb362be
emmisk <koistinenemmisofia@gmail.com>
2021-03-23 14:18:49 +0200

    edit pg and text colors, add hover:underline

--- src/components/ContactInfo.js
+++ src/components/ContactInfo.js
@@ -3,12 +3,12 @@ import React from "react"
 const ContactInfo = () => (
   <>
     <a
-      className="text-center block uppercase text-xl hover:bg-teal-400 py-2 px-4"
+      className="text-center block uppercase text-xl py-2 px-4 text-white bg-black hover:underline"
       href="mailto:koistinenemmisofia@gmail.com"
     >
       koistinenemmisofia@gmail.com
     </a>
-    <h2 className="text-black text-2xl text-center uppercase">
+    <h2 className="text-white bg-black text-1xl text-center uppercase py-2 px-8">
       +358 45 888 1335
     </h2>
   </>
