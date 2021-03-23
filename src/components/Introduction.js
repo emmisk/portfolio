e90f54bf12947e7cb995e914010acac1d890bdb0
emmisk <koistinenemmisofia@gmail.com>
2021-03-23 19:09:03 +0200

    WIP

--- src/components/Introduction.js
+++ src/components/Introduction.js
@@ -1,7 +1,7 @@
 import React from "react"
 
 const Introduction = () => (
-  <p className="text-gray-800 text-xl text-center bg-teal-300 p-8 shadow-inner">
+  <p className="text-gray-800 text-xl text-center bg-teal-300 p-4 shadow-inner">
     Hey, I'm Emmi! This is my portfolio page. You can find my resume and
     projects over here.
   </p>
