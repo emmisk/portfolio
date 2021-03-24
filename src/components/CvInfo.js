1e943f2158400a0622299d453cb2d9522f5c5de4
emmisk <koistinenemmisofia@gmail.com>
2021-03-24 13:19:49 +0200

    WIP

--- src/components/CvInfo.js
+++ src/components/CvInfo.js
@@ -7,43 +7,64 @@ import CvHeader from "./CvHeader"
 const CvInfo = () => {
 
 return (
-    <div className="md:grid grid-cols-2">
+    <div className="md:grid grid-cols-2 p-4">
         <div className="w-full">
             <CvHeader 
                 header="Who am I?"
             />
             <InfoText 
                 infoText="I'm Emmi Koistinen and I living in Helsinki, Finland. 
-                I'm person who inspired by new things all the time and I always want to improve myself.
+                I'm creative person and I love to use my imagination with different things, for example when I painting and coding.
+                I enjoy to learn new things and improve myself.
                 I do my job carefully and always try to do my best. I have good problem solving skills because I like to 
-                reseacrh information and find solutions (I'm also creative person and I love games where I can solve 
-                puzzles)."
+                research information and find solutions (I also love games where I can solve puzzles)."
             />
             <CvHeader 
                 header="My skills"
             />
-            <InfoText 
-                infoText="Programming languages: JavaScript, Java and SQL."
+            <SubHeader 
+            subTitle="Programming languages:"
             />
             <InfoText 
-                infoText="Test automation: JUnit and Selenium. "
+                infoText=" JavaScript and Java."
+            />
+            <SubHeader 
+            subTitle="Test automation:"
             />
             <InfoText 
-                infoText="Marketing and analytics: SEO and making dashboards."
+                infoText="JUnit and Selenium."
+            />
+            <SubHeader 
+            subTitle="Marketing and analytics:"
             />
             <InfoText 
-                infoText="Databases: PostgreSQL."
+                infoText=" SEO and making dashboards."
+            />
+            <SubHeader 
+            subTitle="Databases:"
             />
             <InfoText 
-                infoText="Another skills: google searching."
+                infoText=" PostgreSQL and SQL."
+            />
+            <SubHeader 
+            subTitle="Version control:"
+            />
+            <InfoText 
+                infoText="Git."
+            />
+            <SubHeader 
+            subTitle="Another skills:"
+            />
+            <InfoText 
+                infoText=" google searching."
             />
             <CvHeader 
                 header="Education"
             />
             <SubHeader
                 job="Karelia University of Applied Sciences"
+                subTitle="Business Information Technology 8/2016–4/2020"
                 location ="Joensuu, Finland"
-                titleAndDate="Business Information Technology 8/2016 - 4/2020"
                 infoText="I have done all of my
                 studies remotely so I have a lot of experience about working
                 independently. My thesis subject is Enterprise Application Integration
@@ -54,15 +75,15 @@ return (
             />
             <SubHeader
                 job="STUK - Säteilyturvakeskus"
+                subTitle="System designer 10/2020–4/2021"
                 location ="Helsinki, Finland"
-                titleAndDate="System designer 10/2020 - 4/2021"
                 infoText="I made test automation with Java, JUnit and Selenium. " 
             />
             <br />
             <SubHeader
                 job="Isolta Oy"
+                subTitle="Marketin analyst 8/2018–1/2019"
                 location ="Helsinki, Finland"
-                titleAndDate="Marketin analyst 8/2018 - 1/2019"
                 infoText="I was part of marketing team and made dashboards about marketing data
                 with my teammate. I also develop search engine optimization process." 
             />
@@ -72,6 +93,7 @@ return (
             <InfoText
                 infoText="My favorite hobbies are cycling, painting and playing on Xbox."
             />
+            <br />
         </div>
         <Image />
     </div>
