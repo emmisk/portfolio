d6d4e9221e5a54df5b83e24a8e9096d9b65f8313
emmisk <koistinenemmisofia@gmail.com>
2021-04-07 19:10:12 +0300

    format

--- src/components/CvHeader.js
+++ src/components/CvHeader.js
@@ -1,16 +1,16 @@
 import React from "react"
 
-const CvHeader = ({header, infoText}) => {
-    return (
-        <>
-        <br />
-        <p className="flex-auto text-center font-mono md:w-full font-bold text-2xl text-red-500">
+const CvHeader = ({ header, infoText }) => {
+  return (
+    <>
+      <br />
+      <p className="flex-auto text-center font-mono md:w-full font-bold text-2xl text-red-500">
         {header}
-        </p>
-        <br />
-        <p className="flex-auto text-center font-mono">{infoText}</p>
-        </>
-        )
-    }
+      </p>
+      <br />
+      <p className="flex-auto text-center font-mono">{infoText}</p>
+    </>
+  )
+}
 
 export default CvHeader
