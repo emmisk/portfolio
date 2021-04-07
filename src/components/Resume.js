a21f565bddb784ab0757e699dcffa250453b7344
emmisk <koistinenemmisofia@gmail.com>
2021-04-07 19:10:31 +0300

    format

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -2,9 +2,12 @@ import React from "react"
 
 const Resume = ({ image, alt, handleClick }) => (
   <div className="sm:w-auto">
-  <button onClick={handleClick} className="w-full focus:outline-none shadow-inner transition bg-red-300 hover:bg-red-400">
+    <button
+      onClick={handleClick}
+      className="w-full focus:outline-none shadow-inner transition bg-red-300 hover:bg-red-400"
+    >
       <img src={image} alt={alt} />
-  </button>
+    </button>
   </div>
 )
 
