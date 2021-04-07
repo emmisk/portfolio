25289c2eeffab4982d18ab3f04397b8dce8f99dd
emmisk <koistinenemmisofia@gmail.com>
2021-04-07 19:10:20 +0300

    format

--- src/components/SubHeader.js
+++ src/components/SubHeader.js
@@ -1,14 +1,20 @@
 import React from "react"
 
-const SubHeader = ({job, location, subTitle, infoText}) => {
-    return (
-        <>
-        <p className="flex-auto text-center font-mono md:w-full font-bold text-blue-700">{job}</p>
-        <p className="flex-auto text-center font-mono font-medium text-blue-600">{subTitle}</p>
-        <p className="flex-auto text-center font-mono font-medium text-blue-500">{location}</p>
-        <p className="flex-auto text-center font-mono ml-4 mr-4">{infoText}</p>
-        </>
-        )
-    }
+const SubHeader = ({ job, location, subTitle, infoText }) => {
+  return (
+    <>
+      <p className="flex-auto text-center font-mono md:w-full font-bold text-blue-700">
+        {job}
+      </p>
+      <p className="flex-auto text-center font-mono font-medium text-blue-600">
+        {subTitle}
+      </p>
+      <p className="flex-auto text-center font-mono font-medium text-blue-500">
+        {location}
+      </p>
+      <p className="flex-auto text-center font-mono ml-4 mr-4">{infoText}</p>
+    </>
+  )
+}
 
 export default SubHeader
