dc555063a73d1783f8ce33d1807fea1b8501a13a
emmisk <koistinenemmisofia@gmail.com>
2021-04-12 13:18:10 +0300

    update

--- src/components/Image.js
+++ src/components/Image.js
@@ -2,11 +2,11 @@ import React from "react"
 import MyImg from "../assets/images/me.jpg"
 
 const Image = () => {
-    return (
-        <>
-        <img className=" w-full" src={MyImg} alt="me"/>
-        </>
-        )
-    }
+  return (
+    <>
+      <img className=" w-full" src={MyImg} alt="me" />
+    </>
+  )
+}
 
 export default Image
