4173f5eb1959da1b6316400bf4d7103be6211798
emmisk <koistinenemmisofia@gmail.com>
2021-04-15 12:20:30 +0300

    edit texts

--- src/components/CvInfo.js
+++ src/components/CvInfo.js
@@ -20,15 +20,15 @@ const CvInfo = ({ scroll, setScroll }) => {
       <div className="w-full">
         <CvHeader header="Who am I?" />
         <InfoText
-          infoText="I'm Emmi Koistinen and I living in Helsinki, Finland. 
-                I'm creative person and I love to use my imagination with different things, for example when I painting and coding.
+          infoText="I'm Emmi Koistinen and I live in Helsinki, Finland. 
+                I'm a creative person and I love to use my imagination with different things, for example when I'm painting and coding.
                 I do my job carefully and always try to do my best. I have good problem solving skills because I like to search information over Internet, 
-                talk with others and use my imanigation (I also love games where I can solve puzzles). I have (often) good ideas and I enjoy 
-                to learn new skills and improve myself. My friends say that I have optimistic attitude and I always see a positive side to things."
+                talk with others and use my creativity (I also love games where I can solve puzzles). I have (often) good ideas and I enjoy 
+                learning new skills and improve myself. My friends say that I have optimistic attitude and I always see a positive side to things."
         />
         <CvHeader header="My skills" />
         <SubHeader subTitle="Programming languages:" />
-        <InfoText infoText=" JavaScript and Java." />
+        <InfoText infoText=" JavaScript (React) and Java." />
         <SubHeader subTitle="Test automation:" />
         <InfoText infoText="JUnit and Selenium." />
         <SubHeader subTitle="Marketing and analytics:" />
@@ -41,28 +41,31 @@ const CvInfo = ({ scroll, setScroll }) => {
         <InfoText infoText=" google searching, ideation." />
         <CvHeader header="Education" />
         <SubHeader
-          job="Karelia University of Applied Sciences"
+          title="Karelia University of Applied Sciences"
           subTitle="Business Information Technology 8/2016–4/2020"
           location="Joensuu, Finland"
           infoText="I have done all of my
                 studies remotely so I have a lot of experience about working
-                independently. My thesis subject is Enterprise Application Integration
+                independently. My thesis subject is enterprise application integration
                 and data automation."
         />
         <CvHeader header="Work History" />
         <SubHeader
-          job="STUK - Säteilyturvakeskus"
+          title="STUK - Säteilyturvakeskus"
           subTitle="System designer 10/2020–4/2021"
           location="Helsinki, Finland"
-          infoText="I was part of project where I made test automation using Java, JUnit and Selenium."
+          infoText="My task was to test the laboratory (measurement and analysis) information system and develop test automation. 
+          I executed test automation with Java language and Selenium components. 
+          I also documented the detected error situations
+          in the system."
         />
         <br />
         <SubHeader
-          job="Isolta Oy"
+          title="Isolta Oy"
           subTitle="Marketin analyst 8/2018–1/2019"
           location="Helsinki, Finland"
           infoText="I was part of marketing team and made dashboards about marketing data
-                with my teammate. I also develop search engine optimization process."
+                with my team mate. I also developed search engine optimization process."
         />
         <CvHeader header="My hobbies" />
         <InfoText infoText="My favorite hobbies are cycling, painting and playing on Xbox." />
