bc4cacf41b150d253b1736a2310d7781ec7e3c3e
emmisk <koistinenemmisofia@gmail.com>
2023-08-04 11:21:13 +0300

    edit texts

--- src/components/CvInfo.js
+++ src/components/CvInfo.js
@@ -28,17 +28,17 @@ const CvInfo = ({ scroll, setScroll }) => {
         />
         <CvHeader header="My skills" />
         <SubHeader subTitle="Programming languages:" />
-        <InfoText infoText=" JavaScript (React) and Java." />
+        <InfoText infoText=" JavaScript (React) and HubL syntax" />
         <SubHeader subTitle="Test automation:" />
         <InfoText infoText="JUnit and Selenium." />
-        <SubHeader subTitle="Marketing and analytics:" />
-        <InfoText infoText=" SEO and making dashboards." />
+        <SubHeader subTitle="MarTech:" />
+        <InfoText infoText="HubSpot, SEO, marketing automation." />
         <SubHeader subTitle="Databases:" />
         <InfoText infoText=" PostgreSQL and SQL." />
         <SubHeader subTitle="Version control:" />
         <InfoText infoText="Git." />
         <SubHeader subTitle="Another skills:" />
-        <InfoText infoText="Google searching, ideation." />
+        <InfoText infoText="Web accessibility, Google searching, ideation." />
         <CvHeader header="Education" />
         <SubHeader
           title="Karelia University of Applied Sciences"
@@ -51,21 +51,34 @@ const CvInfo = ({ scroll, setScroll }) => {
         />
         <CvHeader header="Work History" />
         <SubHeader
+          title="Crasman Oy"
+          subTitle="Web developer/HubSpot developer 6/2021–7/2023"
+          location="Helsinki, Finland"
+          infoText="I was a web developer with a focus on JavaScript and HubL syntax, 
+          where accessibility was always a priority. 
+          I excelled in developing custom modules and templates for HubSpot, 
+          aligning content with branding and marketing objectives."
+        />
+        <br />
+        <SubHeader
           title="STUK - Säteilyturvakeskus"
           subTitle="System designer 10/2020–4/2021"
           location="Helsinki, Finland"
-          infoText="My task was to test the laboratory (measurement and analysis) information system and develop test automation. 
-          I executed test automation with Java language and Selenium components. 
-          I also documented the detected error situations
-          in the system."
+          infoText="My primary responsibility was to test the laboratory 
+          (measurement and analysis) information system, with a focus on 
+          developing test automation. I utilized Java and Selenium components 
+          to execute comprehensive test automation, ensuring the system's robustness 
+          and reliability."
         />
         <br />
         <SubHeader
           title="Isolta Oy"
           subTitle="Marketin analyst 8/2018–1/2019"
           location="Helsinki, Finland"
-          infoText="I was part of marketing team and made dashboards about marketing data
-                with my team mate. I also developed search engine optimization process."
+          infoText="I was a part of the marketing team and collaborated with my teammates 
+          to create comprehensive dashboards for analyzing marketing data. Working closely with my team, we 
+          successfully designed and implemented data-driven dashboards that provided 
+          valuable insights into marketing performance and facilitated data-based decision-making."
         />
         <CvHeader header="My hobbies" />
         <InfoText infoText="My favorite hobbies are cycling, painting and playing on Xbox." />
