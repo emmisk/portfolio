293acd411a928abd39058112f46e1bdd1ef85d96
emmisk <koistinenemmisofia@gmail.com>
2023-08-04 12:33:50 +0300

    delete focus

--- src/components/ResumeProject.js
+++ src/components/ResumeProject.js
@@ -5,7 +5,7 @@ const ResumeProject = ({ alt, image, imageColor, handleClick, hoverColor }) => (
     <div className={`col-span-1 text-black text-center bg-black shadow-inner`}>
       <button
         onClick={handleClick}
-        className={`w-full h-auto focus:outline-none shadow-inner transition ${imageColor} hover:${hoverColor}`}
+        className={`w-full h-auto shadow-inner transition ${imageColor} hover:${hoverColor}`}
       >
         <img src={image} alt={alt} className="w-full h-auto" />
       </button>
